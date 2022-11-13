a9193a0522f31f2dc0554b1623f9c2bb36fa2095
4ripley <yud28@ya.ru>
2022-11-13 20:26:49 +0300

    1

--- task-TS.ts
+++ task-TS.ts
@@ -8,3 +8,26 @@ const test2: StartsWith<'a'> = 'abc'; // ok
 const test3: StartsWith<'a'> = 'A'; // error
 // `${T}${string}`;
 
+type KeysDeep<T> = keyof T | (
+    T extends Record<any, infer V>
+        ? V extends Record<any, any>
+            ? KeysDeep<V>
+            : never
+        : never
+);
+
+type Obj = {
+    prop1: string;
+    prop2: {
+        prop3: string;
+    };
+}
+const a: KeysDeep<Obj> = 'prop3';
+
+// type KeysDeep<T> = keyof T | (
+//     T extends Record<any, infer V>
+//         ? V extends Record<any, any>
+//             ? KeysDeep<V>
+//             : never
+//         : never
+// );
